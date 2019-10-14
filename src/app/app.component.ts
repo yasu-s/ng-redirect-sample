dc4387f612ea046ca3a93aedd50da2175b6246c0
Yasuyuki Saito <yasuyuki.saito.2012@gmail.com>
2019-10-14 09:24:02 +0900

    最小構成配置

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,28 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <form #f action="./assets/redirect.html" type="GET">
+      <input type="hidden" name="id" [value]="data.id" />
+      <input type="hidden" name="name" [value]="data.name" />
+      <input type="hidden" name="memo" [value]="data.memo" />
+    </form>
+  `,
 })
-export class AppComponent {}
+export class AppComponent implements AfterViewInit {
+  @ViewChild('f', { static: false }) form: ElementRef<HTMLFormElement> | undefined = undefined;
+
+  data = {
+    id: '1',
+    name: 'Bob',
+    memo: 'memo2',
+  };
+
+  ngAfterViewInit(): void {
+    if (!this.form) {
+      return;
+    }
+    this.form.nativeElement.submit();
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,12 @@
-import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { MenuComponent } from './menu/menu.component';
-import { FormPostComponent } from './redirect/form-post.component';
-import { RedirectComponent } from './redirect/redirect.component';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [AppComponent, FormPostComponent, MenuComponent, RedirectComponent],
-  imports: [BrowserModule, FormsModule, HttpClientModule, AppRoutingModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
