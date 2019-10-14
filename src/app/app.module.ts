128ad45a87d568f0816f4a02b53a5b235b70871f
Yasuyuki Saito <yasuyuki.saito.2012@gmail.com>
2019-10-14 09:38:13 +0900

    不要なモジュール削除

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
 })
