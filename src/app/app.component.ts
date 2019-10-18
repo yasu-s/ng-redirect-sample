6d097dd22e5ab9380bfc6a02e154e54dc96cd179
Yasuyuki Saito <yasuyuki.saito.2012@gmail.com>
2019-10-18 22:47:38 +0900

    method修正

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <form #f action="./assets/redirect.html" type="GET">
+    <form #f action="./assets/redirect.html" method="GET">
       <input type="hidden" name="id" [value]="data.id" />
       <input type="hidden" name="name" [value]="data.name" />
       <input type="hidden" name="memo" [value]="data.memo" />
